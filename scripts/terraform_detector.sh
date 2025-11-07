12c3542586636481b33569ed56137728061c5d20
OmkarHosavalike <homkar8@gmail.com>
2025-11-07 21:23:23 +0530

    Test 2 - Continued

--- scripts/terraform_detector.sh
+++ scripts/terraform_detector.sh
@@ -14,10 +14,12 @@ terraform init -input=false -no-color
 echo "Running terraform plan..."
 set +e
 terraform plan -detailed-exitcode -input=false -no-color -out=tfplan.binary > >(tee "$OUTFILE") 2>&1
-EXIT_CODE=${PIPESTATUS[0]}
+EXIT_CODE_1=${PIPESTATUS[0]}
+EXIT_CODE_2=${PIPESTATUS[1]}
 set -e
 
-echo "terraform plan exit code: $EXIT_CODE"
+echo "terraform plan exit code 1: $EXIT_CODE_1"
+echo "terraform plan exit code 2: $EXIT_CODE_2"
 
 if [[ "$EXIT_CODE" -eq 0 ]]; then
   echo "No drift detected."
